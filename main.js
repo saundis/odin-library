b0795226554e9f255bab10762997ac68ddb81686
saundis <thegnomegaming@gmail.com>
2025-06-11 22:42:10 -0700

    Finished table, adding books, and deleting books functionality

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-const myLibrary = [];
+let myLibrary = [];
 const tbody = document.querySelector('tbody');
-const button = document.querySelector('button');
+const addButton = document.querySelector('.add');
+const dialog = document.querySelector('dialog:first-of-type');
+const editDialog = document.querySelector('dialog:last-of-type');
+const closeButton = document.querySelectorAll('.close');
+const submitButton = document.querySelector('button[type="submit"]');
+
+const titleInput = document.querySelector('input[id="title"]');
+const authorInput = document.querySelector('input[id="author"]');
+const numpagesInput = document.querySelector('input[id="numpages"]');
+const havereadInput = document.querySelector('input[id="haveread"]');
 
 function Book(title, author, numberOfPages, haveRead, id) {
     this.id = id;
@@ -11,37 +20,98 @@ function Book(title, author, numberOfPages, haveRead, id) {
 }
 
 function addBookToLibrary(title, author, numberOfPages, haveRead) {
-    let newBook = new Book(title, author, numberOfPages, haveRead, crypto.randomUUID());
+    let id = crypto.randomUUID()
+    let newBook = new Book(title, author, numberOfPages, haveRead, id);
     myLibrary.push(newBook);
+    displayBooks(newBook, id);
 }
 
-function displayBooks() {
-    for (let book of myLibrary) {
-        let newRow = document.createElement('tr');
+function displayBooks(book, ID) {
+    let newRow = document.createElement('tr');
 
-        let id = document.createElement('td');
-        id.textContent = book.id;
-        newRow.append(id);
+    let trashFill = document.createElement('td');
+    trashFill.classList.add('trash-cell');
+    let trash = document.createElement('img');
+    trash.setAttribute('src', 'img/trash-can.svg');
+    trash.setAttribute('alt', 'Delete row');
+    trash.classList.add('trash');
+    trashFill.append(trash);
+    newRow.append(trashFill);
 
-        let title = document.createElement('td');
-        title.textContent = book.title;
-        newRow.append(title);
 
-        let author = document.createElement('td');
-        author.textContent = book.author;
-        newRow.append(author);
+    let title = document.createElement('td');
+    title.textContent = book.title;
+    newRow.append(title);
 
-        let numberOfPages = document.createElement('td');
-        numberOfPages.textContent = book.numberOfPages;
-        newRow.append(numberOfPages);
+    let author = document.createElement('td');
+    author.textContent = book.author;
+    newRow.append(author);
 
-        let haveRead = document.createElement('td');
-        haveRead.textContent = book.haveRead;
-        newRow.append(haveRead);
+    let numberOfPages = document.createElement('td');
+    numberOfPages.textContent = book.numberOfPages;
+    newRow.append(numberOfPages);
 
-        tbody.append(newRow);
+    let haveReadCell = document.createElement('td');
+    let flexDiv = document.createElement('div');
+    flexDiv.classList.add('read');
+    let statusDiv = document.createElement('div');
+    statusDiv.textContent = book.haveRead;
+    let editCell = document.createElement('img');
+    editCell.setAttribute('src', 'img/pencil.svg');
+    editCell.setAttribute('alt', 'Edit "have read book" status');
+    editCell.classList.add('arrow');
+    flexDiv.append(statusDiv);
+    flexDiv.append(editCell);
+    haveReadCell.append(flexDiv);
+    newRow.append(haveReadCell);
+
+    let id = document.createElement('td');
+    id.textContent = book.id;
+    newRow.append(id);
+
+    tbody.append(newRow);
+
+    function editReadStatus() {
+        editDialog.showModal();
     }
+
+    function deleteRow() {
+        for (let i = 0; i < myLibrary.length; i++) {
+            if (myLibrary[i].id == ID) {
+                console.log('ok');
+                myLibrary.splice(i, 1);
+                newRow.remove();
+                trash.removeEventListener('click', deleteRow);
+                editCell.removeEventListener('click', editReadStatus);
+            }
+        }
+    }
+
+    trash.addEventListener('click', deleteRow);
+    editCell.addEventListener('click', editReadStatus);
 }
 
-addBookToLibrary('test', 'The Tester', 20, false);
-displayBooks();
+submitButton.addEventListener('click', (event) => {
+    titleText = titleInput.value;
+    authorText = authorInput.value;
+    numPagesText = numpagesInput.value;
+    haveReadText = havereadInput.checked;
+    console.log(`${titleText} ${authorText} ${numPagesText} ${haveReadText}`)
+
+    if (titleText !== '' && authorText !== '' && numPagesText !== '') {
+        event.preventDefault()
+        addBookToLibrary(titleText, authorText, numPagesText, haveReadText);
+    }
+})
+
+
+addButton.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+for (let close of closeButton) {
+    close.addEventListener('click', () => {
+        dialog.close();
+        editDialog.close();
+    })
+}
